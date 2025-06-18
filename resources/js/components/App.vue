ff2e3712cff9da3b2856888e47d8f642bc6a396d
Nidheesh J <nidheesh1994@gmail.com>
2025-06-18 16:18:02 +0100

    laravel breeze and user seeder

---
+++ resources/js/components/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="p-6 text-center">
+        <h1 class="text-2xl font-bold">Welcome to Specifi Profit Optimiser</h1>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped></style>
